5b2827039e924b0f1b49d2c72e1d333879f5d40b
Roman Kovalivskiy <roman.kovalivskiy@gmail.com>
2019-09-29 22:36:41 +0300

    Add iterator traits
    IteratorTraits is a struct that should be used to query properties of
    iterator given as a template argument. It is included in C++ standard
    library so it's good to have it here too.
    
    Signed-off-by: Roman Kovalivskiy <roman.kovalivskiy@gmail.com>

---
+++ iterators.h
@@ -0,0 +1,30 @@
+#ifndef ITERATORS_H_INCLUDED
+#define ITERATORS_H_INCLUDED
+
+#include "type_traits.h"
+#include "types.h"
+
+namespace myavr {
+
+template <typename It, typename = Void_t<>>
+struct IteratorTraitsImpl {};
+
+template <typename It>
+struct IteratorTraitsImpl<It, Void_t<typename It::iterator_category,
+                                     typename It::value_type,
+                                     typename It::difference_type,
+                                     typename It::pointer,
+                                     typename It::reference>> {
+    using iterator_category = typename It::iterator_category;
+    using value_type        = typename It::value_type;
+    using difference_type   = typename It::difference_type;
+    using pointer           = typename It::pointer;
+    using reference         = typename It::reference;
+};
+
+template <typename It>
+struct IteratorTraits : public IteratorTraitsImpl<It> {};
+
+}
+
+#endif // ITERATORS_H_INCLUDED
